83e53ac2fadad35b9fcbb3548925ce06cb0da945
Pranav Kulkarni <36969104+freakcap@users.noreply.github.com>
2020-01-28 15:06:07 +0530

    Corrected the Priority algorithm

--- Scheduling algorithms/Priority.java
+++ Scheduling algorithms/Priority.java
@@ -3,17 +3,35 @@ import java.util.Arrays;
 
 public class Priority {
 	void execute(Process[] process,int size) {
-		Arrays.sort(process,new SortByPriority());
-		int sum=0;
+		int sum=0,count=0,highest=0,time=0,min=Integer.MAX_VALUE;
+		boolean check = false;
 		double avgTAT=0,avgWT=0;
-		for(int i=0;i<size;i++) {
-			sum = process[i].CT = sum + process[i].BT;
-			process[i].TAT = process[i].CT - process[i].AT;
-			process[i].WT = process[i].TAT - process[i].BT;
-			avgTAT += process[i].TAT;
-			avgWT += process[i].WT;
-			process[i].display();
-		}
+		while(count<size) {
+					// check=false;
+					for(int i=0;i<size;i++) {
+						if(process[i].AT<=time && process[i].PR<min && process[i].rBT>0) {
+							check=true;
+							highest=i;
+							min=process[highest].PR;
+						}
+					}
+					if(check==false) {
+						time++;
+						continue;
+					}
+					process[highest].rBT--;
+					if(process[highest].rBT==0) {
+						count++;
+						min = Integer.MAX_VALUE;
+						process[highest].CT = time+1;
+						process[highest].TAT = process[highest].CT - process[highest].AT;
+						process[highest].WT = process[highest].TAT - process[highest].BT;
+						avgTAT += process[highest].TAT;
+						avgWT += process[highest].WT;
+						process[highest].display();
+					}
+					time++;
+				}
 		avgTAT = avgTAT/size;
 		avgWT = avgWT/size;
 		System.out.println("\nAvg TAT : "+avgTAT+" and Avg WT : "+avgWT);
